c89d7f41e93c0e6619589aa25e56395c1efc1762
Oto Okino <108208574+okino-oto@users.noreply.github.com>
2023-11-24 11:53:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 function App() {
   const value = 'World';
   return <div>Hello {value}</div>;
+  return <div><a href="/.auth/logout">Log out</a></div>
 }
 
 export default App;
